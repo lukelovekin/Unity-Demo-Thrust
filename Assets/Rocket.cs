bd9b0d8acf0a2d4a218398a4949741f650d85690
Luke <lukelovekin@gmail.com>
2020-09-02 12:16:09 +1000

    more audio and particles

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -9,10 +9,17 @@ public class Rocket : MonoBehaviour {
 
     [SerializeField] float rcsThrust = 200f;
     [SerializeField] float mainThrust = 3f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
 
 
     Rigidbody rigidBody;
     AudioSource audio;
+
+    enum State {Alive, Dying, Transcending}
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -24,27 +31,59 @@ public class Rocket : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        ThrustAndAudio();
-        Steer();
+        if (state == State.Alive)
+        {
+            ThrustAndAudio();
+            Steer();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+
+        if (state != State.Alive) {return;}
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 print("ok");
                 break;
             case "Finish":
-                print("finish");
-                SceneManager.LoadScene(1);
+                SuccessSequence();
                 break;
             default:
-                SceneManager.LoadScene(0);
-                print("dead");
+                DeathSequence();
                 break;
 
         }
+    }
+
+    private void DeathSequence()
+    {
+        state = State.Dying;
+        audio.Stop();
+        audio.PlayOneShot(death);
+        Invoke("RestartGame", 1f);
+    }
+
+    private void SuccessSequence()
+    {
+        state = State.Transcending;
+        audio.Stop();
+        audio.PlayOneShot(success);
+        Invoke("LoadNextScene", 1f); // parameterise time
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+        print("dead");
+    }
+
+    private void LoadNextScene()
+    {
+        print("finish");
+        SceneManager.LoadScene(1); // todo allow more then two levels
     }
 
     private void Steer()
@@ -70,15 +109,20 @@ public class Rocket : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust); // can thrust while rotating
-            if (!audio.isPlaying)
-            {
-                audio.Play();
-            }
+            ApplyThrust();
         }
         else
         {
             audio.Stop();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust); // can thrust while rotating
+        if (!audio.isPlaying)
+        {
+            audio.PlayOneShot(mainEngine);
         }
     }
 }
